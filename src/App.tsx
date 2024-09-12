12953d60d04bd374429ab324a94a5709a0a116aa
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-09-12 23:31:32 +0800

    rectangles points

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,51 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
-  const [rectangleCt, setRectangleCt] = useState(1);
+  const [rectangles, setRectangles] = useState([]);
   const intervalId = useRef<any>(null); //cleanup
+  const [rectanglesPts, setRectanglesPts] = useState(0);
 
   const buyRectangle = () => {
     if (count < 5) {
       return;
     }
     setCount(prev => prev - 5);
-    setRectangleCt(prev => prev + 1);
+    setRectangles(prev => [...prev, { name: 'og' }]);
+    setRectanglesPts(prev => prev + 1);
+  };
+
+  const buyRectangle2 = () => {
+    if (count < 10) {
+      return;
+    }
+    setCount(prev => prev - 10);
+    setRectangles(prev => [...prev, { name: 'foo' }]);
+    setRectanglesPts(prev => prev + 3);
+  };
+
+  const buyRectangle3 = () => {
+    if (count < 20) {
+      return;
+    }
+    setCount(prev => prev - 20);
+    setRectangles(prev => [...prev, { name: 'bar' }]);
+    setRectanglesPts(prev => prev + 8);
   };
 
   useEffect(() => {
     intervalId.current = setInterval(() => {
-      setCount(prev => prev + rectangleCt);
+      setCount(prev => prev + rectanglesPts);
     }, 1000);
     // Cleanup the interval on component unmount or when dependencies change
     return () => clearInterval(intervalId.current);
-  }, [rectangleCt]);
+  }, [rectanglesPts]);
+
+  /*
+    cost        gives
+og  5 blue      1
+foo 10 black    3
+bar 20 yellow   8
+  */
 
   return (
     <>
@@ -40,10 +67,12 @@ function App() {
           }}
         ></div>
         <div>Times clicked: {count}</div>
-        <div>Points added each second: {rectangleCt}</div>
+        <div>Points added each second: {rectanglesPts}</div>
         <button onClick={buyRectangle}>Make rectangle</button>
-        {Array.from({ length: rectangleCt }).map((cur, i) => (
-          <div key={i} className='rectangle'></div>
+        <button onClick={buyRectangle2}>Make rectangle</button>
+        <button onClick={buyRectangle3}>Make rectangle</button>
+        {rectangles.map((cur, i) => (
+          <div key={i} className={cur.name}></div>
         ))}
       </div>
     </>
