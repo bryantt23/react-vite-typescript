40d379687715408eeb39eb1a772a5c625ed8e483
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-09-12 23:22:17 +0800

    timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
@@ -6,6 +6,7 @@ import './App.css';
 function App() {
   const [count, setCount] = useState(0);
   const [rectangleCt, setRectangleCt] = useState(1);
+  const intervalId = useRef<any>(null); //cleanup
 
   const buyRectangle = () => {
     if (count < 5) {
@@ -14,6 +15,14 @@ function App() {
     setCount(prev => prev - 5);
     setRectangleCt(prev => prev + 1);
   };
+
+  useEffect(() => {
+    intervalId.current = setInterval(() => {
+      setCount(prev => prev + rectangleCt);
+    }, 1000);
+    // Cleanup the interval on component unmount or when dependencies change
+    return () => clearInterval(intervalId.current);
+  }, [rectangleCt]);
 
   return (
     <>
@@ -31,6 +40,7 @@ function App() {
           }}
         ></div>
         <div>Times clicked: {count}</div>
+        <div>Points added each second: {rectangleCt}</div>
         <button onClick={buyRectangle}>Make rectangle</button>
         {Array.from({ length: rectangleCt }).map((cur, i) => (
           <div key={i} className='rectangle'></div>
